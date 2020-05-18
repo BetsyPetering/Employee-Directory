b78ef0f5ca20f473643909462b490eddb8469dfb
PinkAttitude77 <betsy.petering@sbcglobal.net>
2020-05-17 21:56:30 -0700

    Updates

---
+++ precache-manifest.f3bfc5953f0e4c4fbdba3148b72a688f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c25247687d07fa4a1d5c33ac4f1cfcb9",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "0658ef78b3c14fc16bc2",
+    "url": "/Employee-Directory/static/css/main.ffc2fe08.chunk.css"
+  },
+  {
+    "revision": "a971c53e01734798a991",
+    "url": "/Employee-Directory/static/js/2.2d6b6474.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.2d6b6474.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0658ef78b3c14fc16bc2",
+    "url": "/Employee-Directory/static/js/main.12ff2beb.chunk.js"
+  },
+  {
+    "revision": "06861812e911b00bea62",
+    "url": "/Employee-Directory/static/js/runtime-main.2e0c67a0.js"
+  }
+]);
